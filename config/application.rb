8ccab4c8959c77f517f70c956725511de22d178a
official-imsny <fatehsingh.sunny5@gmail.com>
2022-09-09 22:26:01 +0530

    add Sidekiq and configuration
    Install Sidekiq and redis gem
    docker compose service
    Related configuration

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module App
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    # NOTE: We can skip this queue adapter as of now
+    # config.active_job.queue_adapter = :sidekiq
   end
 end
